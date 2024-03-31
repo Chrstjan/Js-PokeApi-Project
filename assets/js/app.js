7c6a2c586b348bcbd7519a0bea59fc8a207413ab
Christian <ps5christian@hotmail.com>
2024-03-31 23:07:13 +0200

    Selecting random featured pokemon
    Selecting 3 random pokemons to be displayed on page load

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 //#region GLOBALS
 const app = document.getElementById("app");
 
+let allPokemon = null;
+let randFeatuedPokemon = [];
+
 //Calling functions
 function initApp() {
   getPokemonData();
@@ -18,7 +21,7 @@ function getPokemonData() {
       return res.json();
     })
     .then((json) => {
-      console.log(json);
+      recivedPokemonData(json);
     })
     .catch((error) => {
       console.log("Error fetching pokemon data:", error);
@@ -27,9 +30,37 @@ function getPokemonData() {
 //#endregion model code
 
 //#region controller code
+function recivedPokemonData(pokemonData) {
+  console.log(pokemonData);
+  allPokemon = pokemonData.results;
 
+  randFeatuedPokemon.push(
+    allPokemon[Math.floor(Math.random() * allPokemon.length)],
+    allPokemon[Math.floor(Math.random() * allPokemon.length)],
+    allPokemon[Math.floor(Math.random() * allPokemon.length)]
+  );
+  //   console.log(randFeatuedPokemon);
+  buildFeaturedPokemons(randFeatuedPokemon);
+}
 //#endregion controller code
 
 //#region view code
+function buildFeaturedPokemons(pokemons) {
+  let pokemonCardContainer = document.createElement("div");
+  pokemonCardContainer.classList.add("card-container");
+
+  pokemons.forEach((pokemon) => {
+    let pokemonCard = `
+    <figure class="pokemon-card">
+        <header>
+            <h3>${pokemon.name}</h3>
+        </header>
+    </figure>`;
+
+    pokemonCardContainer.innerHTML += pokemonCard;
+  });
+
+  app.appendChild(pokemonCardContainer);
+}
 
 //#endregion view code
