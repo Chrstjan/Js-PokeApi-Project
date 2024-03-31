c822473af0492474103cbf73ff1e963e9ab88f2b
Christian <ps5christian@hotmail.com>
2024-03-31 22:50:22 +0200

    Fetched pokemon data & base header
    Fetched the pokemon data & made the base layout for the header of the landing page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,29 @@
 //#region GLOBALS
 const app = document.getElementById("app");
 
+//Calling functions
+function initApp() {
+  getPokemonData();
+}
+initApp();
 //#endregion GLOBALS
 
 //#region model code
-
+function getPokemonData() {
+  fetch(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json);
+    })
+    .catch((error) => {
+      console.log("Error fetching pokemon data:", error);
+    });
+}
 //#endregion model code
 
 //#region controller code
